d14a4272b9a77c0a03202a74807d8cb0697d5e8f
Aaron Romeo <aaron@aaronromeo.com>
2021-12-24 08:33:00 -0500

    Temporary sample usage of fetching messages by label id

---
+++ cmd/migrate.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	internal "aaronromeo/mailboxorg/caduceus/internal"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// migrateCmd represents the migrate command
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: runMigrations,
+}
+
+func runMigrations(cmd *cobra.Command, args []string) {
+	fmt.Println("Fetching messages...")
+	labels := []*internal.CadLabel{{
+		Id: "Label_175",
+	}, {
+		Id: "UNREAD",
+	},
+	}
+	messageIDs, err := internal.GetMessagesIDsByLableIDs(labels)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(len(messageIDs), messageIDs)
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// migrateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// migrateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
